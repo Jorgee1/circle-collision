6841183855e4f864b6f3c113e4dfb2f43d0cdb45
Jorgee1 <jorge.ortega.p.1@gmail.com>
2021-10-23 14:14:26 -0500

    Added game loop and speed to the circles

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ var height = 480;
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-var width = canvas.width;
-var height = canvas.height;
-
 var Color = {
     black: '#252525',
-    red: '#C02525'
+    red: '#D05050',
+    blue: '#50D050'
 };
+
 
 class Circle {
     constructor(x, y, r){
         this.x = x;
         this.y = y;
         this.r = r;
+    }
+}
+
+class Entity {
+    constructor(collider, color){
+        this.collider = collider;
+        this.speed = {x:0, y:0};
+        this.color = color;
+    }
+
+    update() {
+        this.collider.x += this.speed.x;
+        this.collider.y += this.speed.y;
     }
 }
 
@@ -31,13 +43,50 @@ function check_circle_collition(c1, c2){
     return d < c1.r + c2.r;
 }
 
+var width = canvas.width;
+var height = canvas.height;
+var n_circle = 50
+var step = 10
 
-var c = new Circle(100, 100, 10)
+var c = new Entity(new Circle(100, 100, 100), Color.red)
 
-context.fillStyle = Color.black;
-context.fillRect(0, 0, width, height);
+var render_list = [c]
+for (let index = 0; index < n_circle; index++) {
+    x = Math.random() * (width - 100) + 50;
+    y = Math.random() * (height - 100) + 50;
 
-draw_circle(context, c, Color.red)
+    var circle = new Circle(x, y, 50);
+    var entity = new Entity(circle, Color.blue);
+    entity.speed.x = 10;
+    entity.speed.y = 10;
+    render_list.push(entity);
+}
+
+function game_loop(){
+
+
+
+    
+    // Update
+    render_list.forEach(circle => {
+        circle.update()
+    })
+
+    // Render
+    context.fillStyle = Color.black;
+    context.fillRect(0, 0, width, height);
+
+    render_list.forEach(circle => {
+        draw_circle(context, circle.collider, circle.color)
+    });
+
+    window.requestAnimationFrame(game_loop);
+}
+
+
+window.requestAnimationFrame(game_loop);
+
+
 
 
 
